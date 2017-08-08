c47d159858a095ebbd74f1243d6d06594de8ea74
Karl Eklund <localpart@gmail.com>
2017-08-08 14:03:24 +0200

    rename variables

--- locking_incrementing.c
+++ locking_incrementing.c
@@ -9,28 +9,29 @@ struct data {
 
 void *increment(void *void_ptr) {
   int i = 0;
-  struct data *dat = (struct data *)void_ptr;
+  struct data *args = (struct data *)void_ptr;
 
   while(++i <= 100000) {
-    pthread_mutex_lock(&dat->mutex);
-    dat->x = dat->x + 1;
-    pthread_mutex_unlock(&dat->mutex);
+    pthread_mutex_lock(&args->mutex);
+    args->x = args->x + 1;
+    pthread_mutex_unlock(&args->mutex);
   }
   return void_ptr;
 }
 
 int main() {
   pthread_t thread0, thread1;
-  struct data d;
+  struct data args;
+  args.x = 0;
 
-  pthread_mutex_init(&d.mutex, NULL);
+  pthread_mutex_init(&args.mutex, NULL);
 
-  pthread_create(&thread0, NULL, increment, &d);
-  pthread_create(&thread1, NULL, increment, &d);
+  pthread_create(&thread0, NULL, increment, &args);
+  pthread_create(&thread1, NULL, increment, &args);
   pthread_join(thread0, NULL);
   pthread_join(thread1, NULL);
 
-  printf("x is now %d\n", d.x);
+  printf("x is now %d\n", args.x);
 
   return 0;
 }
